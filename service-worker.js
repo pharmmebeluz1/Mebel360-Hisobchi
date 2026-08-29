378e5130caa04f0139e1d668ee0a50243b46962f
pharmmebeluz1 <ubaydullayevzuhriddin852@gmail.com>
2026-08-29 16:23:02 +0500

    Mebel360 Hisobchi interfeysi soddalashtirildi. Asosiy ekranda faqat chizma, detallar, kroy, material/furnitura va yakuniy hisob qoldirildi. Qo‘shimcha professional funksiyalar alohida “Professional / Qo‘shimcha” bo‘limiga ko‘chirildi. Funksiyalar o‘chirilmagan, faqat interfeys tartibli va foydalanishga qulay holatga keltirildi.

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='mebel360-hisobchi-v2-equal-sections';
+const CACHE='mebel360-hisobchi-v3-clean-professional';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./assets/icon-192.png','./assets/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
