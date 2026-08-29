2b76792df3c5811add9414941db84a91732dca38
pharmmebeluz1 <ubaydullayevzuhriddin852@gmail.com>
2026-08-29 13:11:31 +0500

    Shkaf ichki bo‘limlarini teng va erkin bo‘lish funksiyasi qo‘shildi. Teng bo‘lish rejimida shkaf eni, material qalinligi va bo‘limlar soniga qarab ichki o‘lchamlar avtomatik hisoblanadi. Masalan: 1900 mm shkaf, 16 mm material, 3 bo‘lim → 612 mm dan teng taqsimlanadi. Chizma va detal hisoblari ham avtomatik yangilanadi

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='mebel360-hisobchi-v1';
+const CACHE='mebel360-hisobchi-v2-equal-sections';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./assets/icon-192.png','./assets/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
